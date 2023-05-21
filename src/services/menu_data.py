eb36de111f30f160ea057e0d22f514f1964f31c3
denis daros <denisv.daros@gmail.com>
2023-05-21 16:34:53 -0300

    req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,21 @@
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+import pandas as pd
 # Req 3
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        df = pd.read_csv(source_path).itertuples(index=False)
+        self.dishes = set()
+        list = {}
+
+        for name, price, ingredient, amount in df:
+            dish = Dish(name, price)
+            ingredient = Ingredient(ingredient)
+
+            if name not in list:
+                list[name] = dish
+                self.dishes.add(dish)
+
+            list[name].add_ingredient_dependency(ingredient, amount)
